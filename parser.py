3d3ef9f3fcff3917dc3767e1291de79b418efaf3
jasonbolanos <jason.bolanos@janeirodigital.com>
2015-11-26 20:11:26 -0600

    adding rules to parser

--- parser.py
+++ parser.py
@@ -4,71 +4,119 @@ def p_main(p):
 
 def p_method_main(p):
 	'method_call : method_cl'
-	print p[1]
+	p[0] = p[1]
 
 def p_method_main_2(p):
 	'method_call : IDENTIFIER block_def | method_cl block_def'
-	print p[1] + p[2]
+	p[0] = p[1] + p[2]
 
 def p_method_cl(p):
 	'method_cl : IDENTIFIER value'
-	print p[1]
+	p[0] = p[1]
 
 def p_method_cl_params(p):
 	'method_cl : IDENTIFIER \'(\' parameter \')\' | IDENTIFIER \'(\' hash_pr \')\''
-	print p[1] + p[2] + p[3] + p[4]
+	p[0] = p[1] + p[2] + p[3] + p[4]
 
 def p_hash_parameter(p):
 	'hash_pr : IDENTIFIER \':\' value \',\' hash_pr'
-	print p[1] + p[2] + p[3] + p[4]
+	p[0] = p[1] + p[2] + p[3] + p[4]
 
 def p_has_parameter_end(p):
 	'hash_pr : IDENTIFIER \':\' value '
-	print p[1] + p[2] + p[3]
+	p[0] = p[1] + p[2] + p[3]
 
 def p_block_def(p):
 	'block_def : DO \'|\' parameter \'|\' new_cmd'
-	print p[1] + p[2] + p[3] + p[4] + p[5]
+	p[0] = p[1] + p[2] + p[3] + p[4] + p[5]
 
 def p_block_def_simple(p):
 	'block_def : \'{\' \'|\' parameter \'|\' ins \'}\''
-	print p[1] + p[2] + p[3] + p[4] + p[5] + p[6]
+	p[0] = p[1] + p[2] + p[3] + p[4] + p[5] + p[6]
 
 def p_class_def(p):
 	'class_def : CLASS name hierachy NEW_LINE TAB symbol_ruby'
-	print p[1] + p[2] + p[3] + p[4] + p[5] + p[6]
+	p[0] = p[1] + p[2] + p[3] + p[4] + p[5] + p[6]
 
 def p_hierachy(p):
 	'hierachy : HIERACHY name'
-	print p[1] + p[2]
+	p[0] = p[1] + p[2]
 
 def p_hierachy_empty(p):
 	'hierachy : '
-	print p[1]
+	p[0] = p[1]
 
 def p_module_def(p):
 	'module_def : MODULE name NEW_LINE TAB symbol_ruby'
-	print p[1] + p[2] + p[3] + p[4] + p[5]
+	p[0] = p[1] + p[2] + p[3] + p[4] + p[5]
 
 def p_method_def(p):
 	'method_def : DEF IDENTIFIER method_s \'(\' method_p \')\' new_cmd '
-	print p[1] + p[2] + p[3] + p[4] + p[5] + p[7]
+	p[0] = p[1] + p[2] + p[3] + p[4] + p[5] + p[7]
 
 def p_method_s(p):
 	'method_s : '!' | '?' | '
-	print p[1]
+	p[0] = p[1]
 
-def p_method_p_assig_cont(p):
+def p_method_pa_assig_cont(p):
 	'method_p : IDENTIFIER \'=\' value \',\' method_p'
-	print p[1] + p[2] + p[3] + p[4] + p[5]
+	p[0] = p[1] + p[2] + p[3] + p[4] + p[5]
 
-def p_method_p_single_cont(p):
-	'method_p : '
+def p_method_pa_single_cont(p):
+	'method_p : IDENTIFIER \',\' method_p'
+	p[0] = p[1] + p[2] + p[3]
 
+def p_method_pa_assig_end(p):
+	'IDENTIFIER \'=\' value'
+	p[0] = p[1] + p[2] + p[3]
 
+def p_method_pa_single_end(p):
+	'method_p : IDENTIFIER'
+	p[0] = p[1]
 
+def	p_ins_single(p):
+	'ins : cmd | exception_def | cond_ins | assig'
+	p[0] = p[1]
 
+def p_inst(p):
+	'ins : NEW_LINE ins'
+	p[0] = p[1] + p[2]
 
+def p_cond_ins(p):
+	'cond_ins : if_def | unless_def | while_def | case_def'
+	p[0] = p[1]
+
+def p_cmd_single(p):
+	'cmd : method_call | vars'
+	p[0] = p[1]
+
+def p_cmd(p):
+	'cmd : vars \'.\' cmd'
+	p[0] = p[1] + p[2] + p[3]
+
+def p_assig(p):
+	'assig : vars ASSIGNATION_OPERATORS cmd | vars ASSIGNATION_OPERATORS value'
+	p[0] = p[1] + p[2] + p[3]
+
+def p_if_def_complex(p):
+	'if_def : if_inst else_def'
+	p[0] = p[1] + p[2]
+
+def p_if_def_simple(p):
+	'condition \'?\' ins \':\' ins'
+	p[0] = p[1] + p[2] + p[3] + p[4]
+
+def p_if_ins(p):
+	'if_inst : IF condition new_cmd'
+	p[0] = p[1] + p[2] + p[3]
+
+def p_unless_def(p):
+	'unless_def : UNLESS condition new_cmd'
+	p[0] = p[1] + p[2] + p[3]
+
+def p_while_def(p):
+	'while_def : WHILE condition new_cmd'
+	p[0] = p[1] + p[2] + p[3]
 
 
 
