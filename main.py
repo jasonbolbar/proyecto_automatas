bd6911cf219dc4f1d327ccd2c9c82025967379b3
jasonbolanos <jason.bolanos@janeirodigital.com>
2015-11-26 20:43:21 -0600

    Add main file

---
+++ main.py
@@ -0,0 +1,18 @@
+# imports 
+import ply.lex as lex
+import ply.yacc as yacc
+
+tokens = ('IDENTIFIER', 'DO', 'CLASS', 'HIERACHY', 'MODULE', 'DEF', 'VALUE', 'ASSIGNATION_OPERATORS',
+ 'IF', 'ELSE', 'UNLESS', 'WHILE', 'FOR', 'IN', 'RANGE', 'ARRAY', 'CASE', 'INSTR', 'WHEN', 'BEGIN', 
+ 'RESCUE', 'STRING', 'FIXNUM', 'NIL', 'FLOAT', 'HASH', 'SYMBOL', 'TRUE', 'FALSE', 'CONSTANT', 
+ 'NAMESPACE_CHARACTER','LOGICAL_OPERATORS', 'COMPARATOR_OPERATORS', 'OPERATORS', 'NEW_LINE', 'TAB',
+ 'INSTACE_VAR', 'CLASS_VAR', 'GLOBAL_VAR', 'PREDEF_VAR'
+ )
+
+import lexer
+
+lex.lex()
+
+import parser
+
+yacc.yacc()
