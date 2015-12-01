7169516049c9668cf6588f1afc361cb4cbc63df0
Carolina Lopez <carolina.lopez@janeirodigital.com>
2015-12-01 15:40:18 -0600

    Concatenate method

--- symbol_coder.py
+++ symbol_coder.py
@@ -1,14 +1,2 @@
-def c_global_variable(p):
-	if len(p) == 3:
-		return p[1] + p[2]	
-	else:
-		return p[1]
-
-def c_class_variable(p):
-	return p[1] + p[2] + p[3]
-
-def c_instance_variable(p):
-	return p[1] + p[2]
-
-def c_variable(p):
-	return p[1]
+def c_concatenate(p):
+	return "".join([p[i] for i in range(1,len(p))])
--- symbol_parser.py
+++ symbol_parser.py
@@ -7,7 +7,7 @@ tokens = symbol_lexer.tokens
 def p_symbol_ruby(p):
 	'''symbol_ruby : variable
 	'''
-	p[0] = p[1]
+	p[0] = symbol_coder.c_concatenate(p)
 	
 
 def p_variable(p):
@@ -15,23 +15,23 @@ def p_variable(p):
 	            | instance_variable 
 	            | class_variable 
 	            | global_variable'''
-	p[0] = symbol_coder.c_variable(p)
+	p[0] = symbol_coder.c_concatenate(p)
 
 def p_instance_variable(p):
 	'''instance_variable : AT IDENTIFIER 
 	                     | AT CONSTANT'''
-	p[0] = symbol_coder.c_instance_variable(p)
+	p[0] = symbol_coder.c_concatenate(p)
 
 def p_class_variable(p):
 	'''class_variable : AT AT IDENTIFIER 
 	                     | AT AT CONSTANT'''
-	p[0] = symbol_coder.c_class_variable(p)
+	p[0] = symbol_coder.c_concatenate(p)
 
 def p_global_variable(p):
 	'''global_variable : DOLLAR IDENTIFIER
 	                     | DOLLAR CONSTANT 
 	                     | SPECIAL_VAR'''
-	p[0] = symbol_coder.c_global_variable(p)                    
+	p[0] = symbol_coder.c_concatenate(p)                    
 	
 
 
