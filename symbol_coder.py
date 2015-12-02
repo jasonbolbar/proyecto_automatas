afa36b59279212202b7ab9da2a7913ae7b0acbba
Carolina Lopez <carolina.lopez@janeirodigital.com>
2015-12-01 19:24:48 -0600

    command

--- symbol_coder.py
+++ symbol_coder.py
@@ -5,4 +5,4 @@ def c_concatenate_by_index(p, begin, end):
 	return "".join([p[i] for i in range(begin,end+1)])
 
 def c_method(p):
-	return 'def' + c_concatenate_by_index(p,2,7) + '\nend'	
+	return 'def' + c_concatenate_by_index(p,2,9) + '\nend'	
